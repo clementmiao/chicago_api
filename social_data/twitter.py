ef1771afbef2e8fc162a35ec2d26cb27af5539c1
Clement Miao <clementmiao@gmail.com>
2014-02-28 09:52:56 -0600

    fixing TweepError

--- social_data/twitter.py
+++ social_data/twitter.py
@@ -12,23 +12,21 @@ access_token_secret='kOgg3N9hgWxLigi9yfIoShqrn6l3kgyvRDOiK8chtnC2M'
 # auth.set_access_token(access_token_key, access_token_secret)
 # api = tweepy.API(auth)
 
-def twitter():
-        # print "Hello Bob"
+# def twitter():
+#         # print "Hello Bob"
         
         
-        l = api.search(geocode = "41.8954,-87.6243,60km", count = 10000)
-        # list = api.home_timeline()
-        s = Service.objects.get(name="twitter")
-        for x in l:
-            if x.geo != None:
-                lat = x.geo["coordinates"][0]
-                lon = x.geo["coordinates"][1]  
-                find = Post.objects.filter(identifier = x.id, service = s)
-                if len(find) == 0:
-                    post = Post(service = s, latitude = lat, longitude = lon, identifier = x.id, text = x.text, link = "", image = "", timestamp = x.created_at)
-                    post.save()
+#         l = api.search(geocode = "41.8954,-87.6243,60km", count = 10000)
+#         # list = api.home_timeline()
+#         s = Service.objects.get(name="twitter")
+#         for x in l:
+#             if x.geo != None:
+#                 lat = x.geo["coordinates"][0]
+#                 lon = x.geo["coordinates"][1]  
+#                 find = Post.objects.filter(identifier = x.id, service = s)
+#                 if len(find) == 0:
+#                     post = Post(service = s, latitude = lat, longitude = lon, identifier = x.id, text = x.text, link = "", image = "", timestamp = x.created_at)
+#                     post.save()
         # print "Done for now"
 
-# def listen():
-#     track = 
     
